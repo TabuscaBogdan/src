ed503037de0150a8dab0c0149e18f0cc8c458f8e
Bogdan <bogdan.tabusca@gmail.com>
2017-04-01 19:50:47 +0300

    Regular Poly
    The mysteries of graphics still elude me.

--- shapes/Canvas.java
+++ shapes/Canvas.java
@@ -1,7 +1,17 @@
 package shapes;
 
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Random;
+
 /**
  * Created by Bogdan on 01.04.2017.
  */
-public class Canvas {
+public class Canvas extends JPanel {
+    public void drawShapeAt(int x, int y) {
+        /*Graphics2D graphics2D = (Graphics2D) grap;
+        rand = new Random();
+        graphics.setColor(new Color(rand.nextInt(800000)));*/
+    }
 }
